b7d4aebeee1f8eb5786303b80b574b59ee3b3ad4
fvalka <fvalka@vektorraum.com>
2017-05-20 13:37:18 +0200

    Add Metars integration Test

--- src/test/scala/com/vektorraum/aviatorsbot/weather/AddsWeatherServiceTest.scala
+++ src/test/scala/com/vektorraum/aviatorsbot/weather/AddsWeatherServiceTest.scala
@@ -1,6 +1,11 @@
 package com.vektorraum.aviatorsbot.weather
 
+import java.util.concurrent.TimeUnit
+
 import org.scalatest.FunSuite
+
+import scala.concurrent._
+import scala.concurrent.duration.Duration
 
 /**
   * Created by fvalka on 20.05.2017.
@@ -9,7 +14,8 @@ class AddsWeatherServiceTest extends FunSuite {
 
   test("testGetMetars") {
     val cut = new AddsWeatherService()
-    val metars = cut.getMetars(List("LOWW", "KJFK"))
+    val metarsFuture = cut.getMetars(List("LOWW", "KJFK"))
+    val metars = Await.result(metarsFuture, Duration(10000, TimeUnit.MILLISECONDS))
     println(metars)
   }
 
