e2cbd9b3d8fe627ee639a6cd00bd89d37942c63f
fvalka <fvalka@vektorraum.com>
2018-08-17 12:23:03 +0200

    Unit test for help command

---
+++ src/test/scala/com/vektorraum/aviatorsbot/bot/AviatorsBotHelpTest.scala
@@ -0,0 +1,60 @@
+package com.vektorraum.aviatorsbot.bot
+
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.{FeatureSpec, GivenWhenThen}
+import org.scalatest.concurrent.Eventually
+import org.scalatest.time.{Millis, Span}
+import org.scalatest.Matchers._
+
+class AviatorsBotHelpTest extends FeatureSpec with GivenWhenThen with MockFactory with Eventually {
+  info("As a pilot I want to")
+  info("Be able to see which commands I can use on this bot")
+
+  implicit override val patienceConfig: PatienceConfig =
+    PatienceConfig(timeout = scaled(Span(1000, Millis)), interval = scaled(Span(60, Millis)))
+
+  feature("Help command shows all available commands") {
+    scenario("Pilot sends just the help command") {
+      Given("AviatorsBot without any mocks")
+      val bot = new AviatorsBotForTesting()
+
+      When("Pilot sends the help command")
+      bot.receiveMockMessage("/help")
+
+      Then("Help message is received")
+      eventually {
+        bot.replySent should include ("/rm")
+        bot.replySent should include ("/ls")
+      }
+    }
+
+    scenario("Help command works with any arguments") {
+      Given("AviatorsBot without any mocks")
+      val bot = new AviatorsBotForTesting()
+
+      When("Pilot sends the help command")
+      bot.receiveMockMessage("/help asd 949 xjsjsj")
+
+      Then("Help message is received")
+      eventually {
+        bot.replySent should include ("/rm")
+        bot.replySent should include ("/ls")
+      }
+    }
+
+    scenario("Help command works without prefix in front") {
+      Given("AviatorsBot without any mocks")
+      val bot = new AviatorsBotForTesting()
+
+      When("Pilot sends the help command")
+      bot.receiveMockMessage("help")
+
+      Then("Help message is received")
+      eventually {
+        bot.replySent should include ("/rm")
+        bot.replySent should include ("/ls")
+      }
+    }
+  }
+
+}
