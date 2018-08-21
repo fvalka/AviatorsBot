ff0db18c655d03b5a009318152f7d8b8296a1415
fvalka <fvalka@vektorraum.com>
2018-08-21 11:03:23 +0200

    Improved unit tests and documentation

--- src/main/scala/com/vektorraum/aviatorsbot/calculations/DensityAltitude.scala
+++ src/main/scala/com/vektorraum/aviatorsbot/calculations/DensityAltitude.scala
@@ -1,7 +1,7 @@
 package com.vektorraum.aviatorsbot.calculations
 
 import squants.MetricSystem
-import squants.motion.{Bars, Pressure}
+import squants.motion.{Bars, Pascals, Pressure}
 import squants.space.{Feet, Length}
 import squants.thermal.{Celsius, Kelvin, Rankine, Temperature}
 
@@ -13,12 +13,15 @@ object DensityAltitude {
     * Based upon the equations published by NOAA on weather.gov:
     * https://www.weather.gov/media/epz/wxcalc/densityAltitude.pdf
     *
-    * @param stationPressure Station Pressure, not altimeter setting/QNH
+    * @param stationPressure Actual station pressure, not altimeter setting/QNH
     * @param temperature Temperature at pressureAlt
-    * @param dewpoint Dewpoint at pressureAlt
+    * @param dewpoint Dew point at pressureAlt
     * @return Density altitude
     */
   def apply(stationPressure: Pressure, temperature: Temperature, dewpoint: Temperature): Length = {
+    require(temperature >= dewpoint)
+    require(stationPressure > Pascals(0))
+
     val e = vaporPressure(dewpoint)
     val T_v = virtualTemperature(temperature, e, stationPressure)
     val base = 511.6 * (stationPressure.to(Bars)/T_v.to(Rankine))
@@ -26,9 +29,24 @@ object DensityAltitude {
     Feet(145366) * (1 - Math.pow(base, 0.235))
   }
 
+  /**
+    * Calculates the vapor pressure for a given dew point
+    *
+    * @param dewpoint Dew point for vapor pressure calculation
+    * @return Vapor pressure for this dew point
+    */
   protected def vaporPressure(dewpoint: Temperature): Pressure =
-    Bars(6.11  * MetricSystem.Milli) * math.pow(10, (7.5 * dewpoint.in(Celsius))/(Celsius(237.7) + dewpoint.in(Celsius)))
+    Bars(6.11  * MetricSystem.Milli) *
+      math.pow(10, (7.5 * dewpoint.in(Celsius))/(Celsius(237.7) + dewpoint.in(Celsius)))
 
+  /**
+    * Calculates the virtual temperature required for the density altitude calculation
+    *
+    * @param temperature Temperature at pressureAlt
+    * @param vaporPressure Vapor pressure
+    * @param stationPressure Station Pressure, not altimeter setting/QNH
+    * @return
+    */
   protected def virtualTemperature(temperature: Temperature,
                                    vaporPressure: Pressure, stationPressure: Pressure): Temperature =
     (temperature in Kelvin) / (1-(vaporPressure/stationPressure)*(1-0.622))
--- src/test/scala/com/vektorraum/aviatorsbot/calculations/DensityAltitudeTest.scala
+++ src/test/scala/com/vektorraum/aviatorsbot/calculations/DensityAltitudeTest.scala
@@ -1,14 +1,12 @@
 package com.vektorraum.aviatorsbot.calculations
 
-import com.codahale.metrics.MetricRegistry.MetricSupplier
-import com.vektorraum.aviatorsbot.bot.util.TimeUtil
-import org.scalatest.{FunSuite, GivenWhenThen}
-import org.scalatest.prop.TableDrivenPropertyChecks
 import org.scalatest.Matchers._
-import squants.{MetricSystem, Temperature}
+import org.scalatest.prop.TableDrivenPropertyChecks
+import org.scalatest.{FunSuite, GivenWhenThen}
+import squants.MetricSystem
 import squants.motion.Bars
-import squants.space.{Feet, Length}
-import squants.thermal.Celsius
+import squants.space.{Feet, Length, Meters}
+import squants.thermal.{Celsius, Fahrenheit}
 
 class DensityAltitudeTest extends FunSuite with TableDrivenPropertyChecks with GivenWhenThen {
   implicit val tolerance: Length = Feet(3)
@@ -19,13 +17,25 @@ class DensityAltitudeTest extends FunSuite with TableDrivenPropertyChecks with G
     // https://www.weather.gov/epz/wxcalc_densityaltitude
     val properties = Table(
       ("stationPressure", "temperature", "dewpoint", "densityAlt"),
-      (Bars(MetricSystem.Milli * 1013.0), Celsius(30.0), Celsius(20.0), Feet(2044.3))
+      (Bars(MetricSystem.Milli * 1013.0), Celsius(30.0), Celsius(20.0), Feet(2044.3)),
+      (Bars(1.013), Fahrenheit(90.0), Fahrenheit(60.0), Meters(676.1)),
+      (Bars(0.8), Celsius(50.0), Fahrenheit(10.0), Feet(11559.6))
     )
 
     Then("Only valid ones are identified as such")
     forAll (properties) { (stationPressure, temperature, dewpoint, densityAlt) =>
       DensityAltitude(stationPressure, temperature, dewpoint) ≈ densityAlt shouldEqual true
     }
+  }
+
+  test("Invalid dewpoint leads to an exception") {
+    an[IllegalArgumentException] should be thrownBy
+      DensityAltitude(Bars(MetricSystem.Milli * 1013.0), Celsius(30.0), Celsius(31.0))
+  }
+
+  test("Negative pressures are not allowed") {
+    an[IllegalArgumentException] should be thrownBy
+      DensityAltitude(Bars(MetricSystem.Milli * -1013.0), Celsius(30.0), Celsius(20.0))
   }
 
 }
