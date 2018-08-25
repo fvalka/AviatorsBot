97adab8f4a4688f2abee664e836961bcfdbee394
fvalka <fvalka@vektorraum.com>
2018-08-25 13:16:52 +0200

    Fixed missing string endings for merge

--- scripts/install_package.sh
+++ scripts/install_package.sh
@@ -6,14 +6,14 @@ CONFIG_DIR=/etc/aviatorsbot-scala
 if [ -e "${CONFIG_DIR}/aviatorsbot.conf-changed" ]
 then
     if [ ! -e "${CONFIG_DIR}/aviatorsbot.conf-base" ]
-     then cp "${CONFIG_DIR}/aviatorsbot.conf ${CONFIG_DIR}/aviatorsbot.conf-base"
+     then cp "${CONFIG_DIR}/aviatorsbot.conf" "${CONFIG_DIR}/aviatorsbot.conf-base"
     fi
 
-    cp "${CONFIG_DIR}/aviatorsbot.conf-base ${CONFIG_DIR}/aviatorsbot.conf-new-base"
+    cp "${CONFIG_DIR}/aviatorsbot.conf-base" "${CONFIG_DIR}/aviatorsbot.conf-new-base"
 
-    git merge-file --theirs "${CONFIG_DIR}/aviatorsbot.conf ${CONFIG_DIR}/aviatorsbot.conf-base ${CONFIG_DIR}/aviatorsbot.conf-changed"
+    git merge-file --theirs "${CONFIG_DIR}/aviatorsbot.conf" "${CONFIG_DIR}/aviatorsbot.conf-base" "${CONFIG_DIR}/aviatorsbot.conf-changed"
 
-    mv "${CONFIG_DIR}/aviatorsbot.conf-new-base ${CONFIG_DIR}/aviatorsbot.conf-base"
+    mv "${CONFIG_DIR}/aviatorsbot.conf-new-base" "${CONFIG_DIR}/aviatorsbot.conf-base"
 fi
 
 service aviatorsbot-scala restart
