3106219000a39a030cedeb2e63f32c80c1706aa4
fvalka <fvalka@vektorraum.com>
2018-08-25 13:02:53 +0200

    Added merging of configuration files, since dpkg doesn't recognize the file as a configuration file, because its a symlink

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 mkdir -p /tmp/aviatorsbot-scala-deploy
+
+if [ -e /etc/aviatorsbot-scala/aviatorsbot.conf ]
+ then cp /etc/aviatorsbot-scala/aviatorsbot.conf /etc/aviatorsbot-scala/aviatorsbot.conf-changed
+fi
--- scripts/install_package.sh
+++ scripts/install_package.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 dpkg -i /tmp/aviatorsbot-scala-deploy/target/aviatorsbot-scala_1.0_all.deb
+
+CONFIG_DIR=/etc/aviatorsbot-scala
+
+if [ -e "${CONFIG_DIR}/aviatorsbot.conf-changed" ]
+then
+    if [ ! -e "${CONFIG_DIR}/aviatorsbot.conf-base" ]
+     then cp "${CONFIG_DIR}/aviatorsbot.conf ${CONFIG_DIR}/aviatorsbot.conf-base"
+    fi
+
+    cp "${CONFIG_DIR}/aviatorsbot.conf-base ${CONFIG_DIR}/aviatorsbot.conf-new-base"
+
+    git merge-file --theirs "${CONFIG_DIR}/aviatorsbot.conf ${CONFIG_DIR}/aviatorsbot.conf-base ${CONFIG_DIR}/aviatorsbot.conf-changed"
+
+    mv "${CONFIG_DIR}/aviatorsbot.conf-new-base ${CONFIG_DIR}/aviatorsbot.conf-base"
+fi
+
+service aviatorsbot-scala restart
