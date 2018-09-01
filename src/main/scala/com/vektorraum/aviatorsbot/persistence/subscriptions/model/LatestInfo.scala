d14f8151bb2b3e0d1056d99efd0a61b6c11aff84
fvalka <fvalka@vektorraum.com>
2018-09-01 16:37:23 +0200

    Improved documentation for model case classes

--- src/main/scala/com/vektorraum/aviatorsbot/persistence/subscriptions/model/LatestInfo.scala
+++ src/main/scala/com/vektorraum/aviatorsbot/persistence/subscriptions/model/LatestInfo.scala
@@ -3,16 +3,10 @@ package com.vektorraum.aviatorsbot.persistence.subscriptions.model
 import java.util.Date
 
 /**
-  * _    _                                       
-  * | |  | |                                      
-  * __   _____| | _| |_ ___  _ __ _ __ __ _ _   _ _ __ ___  
-  * \ \ / / _ \ |/ / __/ _ \| '__| '__/ _` | | | | '_ ` _ \ 
-  * \ V /  __/   <| || (_) | |  | | | (_| | |_| | | | | | |
-  * \_/ \___|_|\_\\__\___/|_|  |_|  \__,_|\__,_|_| |_| |_|
+  * Information about the update already successfully sent to the subscriber
   *
-  * vektorraum.com
-  *
-  * Created by fvalka on 27.05.2017.
+  * @param issuedAt Issuance time of the message (METAR, TAF, etc.) already sent to the user
+  * @param hash Mumur3Hash of the message sent to the subscriber
   */
 case class LatestInfo(issuedAt: Date, hash: Int) {
 
--- src/main/scala/com/vektorraum/aviatorsbot/persistence/subscriptions/model/Subscription.scala
+++ src/main/scala/com/vektorraum/aviatorsbot/persistence/subscriptions/model/Subscription.scala
@@ -3,18 +3,15 @@ package com.vektorraum.aviatorsbot.persistence.subscriptions.model
 import java.util.Date
 
 /**
-  *            _    _
-  *           | |  | |
-  * __   _____| | _| |_ ___  _ __ _ __ __ _ _   _ _ __ ___  
-  * \ \ / / _ \ |/ / __/ _ \| '__| '__/ _` | | | | '_ ` _ \ 
-  *  \ V /  __/   <| || (_) | |  | | | (_| | |_| | | | | | |
-  *   \_/ \___|_|\_\\__\___/|_|  |_|  \__,_|\__,_|_| |_| |_|
+  * Subscription of one subscriber, identified by their chatId, to one station, identified by its ICAO code.
   *
-  * vektorraum.com
-  *
-  * Created by fvalka on 27.05.2017.
+  * @param chatId Subscribers chatId
+  * @param icao Station to which the subscriber subscribed
+  * @param validUntil Expires after this date passed
+  * @param metar Whether the subscriber subscribed to METARs
+  * @param taf Whether the subscriber subscribed to TAFs
+  * @param latestMetar Information about the latest METAR already successfully sent to the subscriber
+  * @param latestTaf Information about the latest TAF already sent to the subscriber
   */
 case class Subscription(chatId: Long, icao: String, validUntil: Date, metar: Boolean = true, taf: Boolean = true,
-                        var latestMetar: Option[LatestInfo] = None, var latestTaf: Option[LatestInfo] = None) {
-
-}
+                        var latestMetar: Option[LatestInfo] = None, var latestTaf: Option[LatestInfo] = None)
