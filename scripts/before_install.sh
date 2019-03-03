1b3054e1ee5d71fa4427fd30f003ce0194a222e4
Fabian Valka <fvalka@vektorraum.com>
2019-03-03 23:03:02 +0100

    Extended charts for mongodb service connection

--- scripts/before_install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir -p /tmp/aviatorsbot-scala-deploy
-
-if [ -e /etc/aviatorsbot-scala/aviatorsbot.conf ]
- then mv /etc/aviatorsbot-scala/aviatorsbot.conf /etc/aviatorsbot-scala/aviatorsbot.conf-changed
-fi
