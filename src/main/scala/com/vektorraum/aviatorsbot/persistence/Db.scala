4bddccf689d75bc77ea8d6156c551f03d6cc85c9
Fabian Valka <fvalka@vektorraum.com>
2019-03-30 00:46:00 +0100

    Changed database connection mechanism

--- src/main/scala/com/vektorraum/aviatorsbot/persistence/Db.scala
+++ src/main/scala/com/vektorraum/aviatorsbot/persistence/Db.scala
@@ -3,25 +3,25 @@ package com.vektorraum.aviatorsbot.persistence
 import com.typesafe.config.Config
 import reactivemongo.api.{DefaultDB, MongoConnection, MongoDriver}
 
+import scala.collection.JavaConverters._
+import scala.collection.mutable
 import scala.concurrent.Future
-import scala.util.Try
 
 /**
   * Database connector for reactive mongo
   */
 class Db(config: Config) {
   protected val dbConfig: Config = config.getConfig("db")
-  protected val mongoUri: String = dbConfig.getString("url")
 
   import scala.concurrent.ExecutionContext.Implicits.global // use any appropriate context
 
   // Connect to the database: Must be done only once per application
   val driver = MongoDriver()
-  val parsedUri: Try[MongoConnection.ParsedURI] = MongoConnection.parseURI(mongoUri)
-  val connection: Try[MongoConnection] = parsedUri.map(driver.connection)
+  private val hosts: mutable.Buffer[String] = dbConfig.getStringList("hosts").asScala
+  private val connection: MongoConnection = driver.connection(hosts)
 
   // Database and collections: Get references
-  val futureConnection: Future[MongoConnection] = Future.fromTry(connection)
+  val futureConnection: Future[MongoConnection] = Future.successful(connection)
 
   def aviatorsDb: Future[DefaultDB] = futureConnection.flatMap(_.database(dbConfig.getString("database")))
 
